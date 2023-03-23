54caa7e4bc4a2afc9aa01c0840c8632236861ab5
Trevor Wiers <trevor.t.wiers@gmail.com>
2023-03-23 14:48:35 -0500

    added project pages and code link

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('grit-eq', views.gritEQ, name="gritEQ"),
+    path('spectro', views.spectro, name="spectro")
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, "main/home.html")
 
+def gritEQ(request):
+    return render(request, "main/grit-eq.html")
+
+def spectro(request):
+    return render(request, "main/spectro.html")
