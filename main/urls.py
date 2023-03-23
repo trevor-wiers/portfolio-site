cbd25b986429be0bbb3eb619b819576ff215ac35
Trevor Wiers <trevor.t.wiers@gmail.com>
2023-03-23 15:55:25 -0500

    added home button, filled project pages

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('home', views.home, name="home"),
     path('grit-eq', views.gritEQ, name="gritEQ"),
     path('spectro', views.spectro, name="spectro")
 ]
